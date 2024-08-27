bf405ac9ffdf5d7c4b9df60a1fa4c74c4b855e8f
MarceliBil <marbil003@gmail.com>
2024-08-27 14:24:27 +0200

    code refactor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-
+// flags
 let isPaused = false;
 let isBackground = true;
 let isHelpers = true;
@@ -11,8 +11,7 @@ const scene = new THREE.Scene();
 
 // camera
 const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.0001, 10000);
-camera.position.set(window.innerWidth < 576 ? -70 : 10, 20, 70);
-console.log(camera.position);
+camera.position.set(window.innerWidth < 576 ? -70 : 10, 20, 70);;
 
 // renderer
 const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
@@ -29,30 +28,37 @@ const axesHelper = new THREE.AxesHelper(100);
 
 scene.add(gridHelper, axesHelper);
 
+// creating functions
+const loadTexture = (path) => new THREE.TextureLoader().load(path);
 
-// texture loader
-const textureLoader = new THREE.TextureLoader();
+const createMaterial = (texturePath, materialType = 'Phong') => {
+    const texture = loadTexture(texturePath);
+    const materials = {
+        Basic: THREE.MeshBasicMaterial,
+        Phong: THREE.MeshPhongMaterial,
+    };
+    
+    return new materials[materialType]({ map: texture });
+};
 
-const sunTexture = textureLoader.load('images/sun-texture.jpg');
-const earthTexture = textureLoader.load('images/earth-texture.jpg');
-const moonTexture = textureLoader.load('images/moon-texture.jpg');
+const createMesh = (geometry, material) => new THREE.Mesh(geometry, material);
 
-const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
-const earthMaterial = new THREE.MeshPhongMaterial({ map: earthTexture });
-const moonMaterial = new THREE.MeshPhongMaterial({ map: moonTexture });
+// creating spheres
+const sunMaterial = createMaterial('images/sun-texture.jpg', 'Basic');
+const earthMaterial = createMaterial('images/earth-texture.jpg', 'Phong');
+const moonMaterial = createMaterial('images/moon-texture.jpg', 'Phong');
 
 const sunGeometry = new THREE.SphereGeometry(12, 32, 32);
 const earthGeometry = new THREE.SphereGeometry(5, 32, 32);
 const moonGeometry = new THREE.SphereGeometry(1.5, 32, 32);
 
-const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
-const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
-const moonMesh = new THREE.Mesh(moonGeometry, moonMaterial);
+const sunMesh = createMesh(sunGeometry, sunMaterial);
+const earthMesh = createMesh(earthGeometry, earthMaterial);
+const moonMesh = createMesh(moonGeometry, moonMaterial);
 
 sunMesh.position.set(0, 0, 0);
 earthMesh.position.set(35, 0, 0);
 earthMesh.rotation.z = THREE.MathUtils.degToRad(23.5);
-
 
 const moonOrbitPivot = new THREE.Object3D();
 moonOrbitPivot.add(moonMesh);
@@ -69,25 +75,25 @@ moonMesh.position.set(10, 0, 0);
 
 scene.add(sunMesh);
 
-// Renderer configuration
+// renderer configuration
 renderer.shadowMap.enabled = true;
 
-// Lighting configuration
+// lighting configuration
 const sunLight = new THREE.PointLight(0xffffff, 1200, 200);
-sunLight.position.set(0, 0, 0);
 sunLight.castShadow = true;
 scene.add(sunLight);
 
 const ambientLight = new THREE.AmbientLight(0x404040, 0.3);
 scene.add(ambientLight);
 
-// Mesh shadow configuration;
+// mesh shadow configuration;
 earthMesh.castShadow = true;
 earthMesh.receiveShadow = true;
 moonMesh.castShadow = true;
 moonMesh.receiveShadow = true;
 
 // scene background
+const textureLoader = new THREE.TextureLoader();
 const sceneBackgroundTexture = textureLoader.load('images/space-bg.png');
 sceneBackgroundTexture.encoding = THREE.sRGBEncoding;
 scene.background = sceneBackgroundTexture;
@@ -108,7 +114,6 @@ function animate() {
 }
 animate();
 
-
 // handle resize
 window.addEventListener('resize', () => {
     const width = window.innerWidth;
@@ -118,7 +123,6 @@ window.addEventListener('resize', () => {
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
 });
-
 
 // buttons / controls
 const pauseToggle = document.querySelector('#pause');
@@ -140,4 +144,3 @@ backgroundToggle.addEventListener('click', () => {
 });
 
 resetBtn.addEventListener('click', () => location.reload());
-
